3f6c1a6de49538c543f459d013d85fcdb5ee41e8
ianpsa <ian.simao@sou.inteli.edu.br>
2025-11-06 15:40:18 -0300

    Configure static export for GitHub Pages - Remove API routes incompatible with static export

--- docs/security-docs/next.config.mjs
+++ docs/security-docs/next.config.mjs
@@ -11,8 +11,8 @@ const config = {
   },
   // Configuração para GitHub Pages
   // Se o repositório não for username.github.io, descomente e configure o basePath
-  // basePath: '/2025-2B-T12-EC08-SecDoc',
-  // assetPrefix: '/2025-2B-T12-EC08-SecDoc',
+  basePath: '/2025-2B-T12-EC08-SecDoc',
+  assetPrefix: '/2025-2B-T12-EC08-SecDoc/',
 };
 
 export default withMDX(config);
