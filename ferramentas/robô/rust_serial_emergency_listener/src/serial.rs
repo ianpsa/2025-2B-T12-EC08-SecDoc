13caffa2df978b55ecf29b508f18cf9fb5b3ab25
ianpsa <ian.simao@sou.inteli.edu.br>
2025-12-03 11:40:34 -0300

    fix: serial values reception

--- ferramentas/robô/rust_serial_emergency_listener/src/serial.rs
+++ ferramentas/robô/rust_serial_emergency_listener/src/serial.rs
@@ -98,12 +98,11 @@ impl SerialHandler {
                         last_valid_read_time = std::time::Instant::now();
                     }
                     
-                    tokio::time::sleep(Duration::from_secs(2)).await;
+                    tokio::time::sleep(Duration::from_secs(1)).await;
                 }
             }
             
             // Delay mínimo para não sobrecarregar CPU
-            tokio::time::sleep(Duration::from_millis(10)).await;
         }
     }
 }
